1dbba7614361180a03496e96d55d0bc07eaec3a3
Leeco <leeco19@qq.com>
2022-10-13 18:59:24 +0800

    I hope you can work

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import { Header } from '../Header'
+import { Footer } from '../Footer'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export const Layout: NextPage<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  )
+}
